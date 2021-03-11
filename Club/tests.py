14d7aa827bd713c59122a9fad6909fefbd2f0cde
dungtomcao <tomcao090@gmail.com>
2021-03-10 17:38:58 -0800

    Assignment 10 Commit

--- Club/tests.py
+++ Club/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import clubMeeting, meetingMinutes, event, resource
 import datetime
+from django.urls import reverse, reverse_lazy
+
 # Create your tests here.
 class clubMeetingtest(TestCase):
     def setUp(self):
@@ -47,3 +49,27 @@ class eventtest(TestCase):
 
     def test_tableneame(self):
         self.assertEqual(str(event._meta.db_table), 'event')
+
+class new_resource_authentication_test(TestCase):
+    def setUp(self):
+        self.type=resource(resourcename='resource1', resourcetype='type1', resourceurl='url1', dateentered=datetime.date(2021, 1, 2),userid=User(username='testuser1'), resourcedescription='a resource')
+        self.test_user=User.objects.create(username='testuser1', password='P@ssw0rd1')
+
+    def test_redirect_if_not_logged_in(self):
+        response=self.client.get(reverse('newresource'))
+        self.assertRedirects(response, '/accounts/login/?next=/Club/newResource/')
+
+'''ERROR: test_logged_in_uses_correct_template (Club.tests.new_resource_authentication_test)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "/Users/elisacao/Desktop/SCC doc/IT112/djangoprojects/PythonClub_W2021/Club/tests.py", line 65, in test_logged_in_uses_correct_template
+    self.assertEqual(str(response.context['user']), 'testuser1')
+TypeError: 'NoneType' object is not subscriptable'''
+
+'''I have been doing research on why this error happens and how to fix it, as well as trying out a couple solutions to no avail. Please help. Thank you'''
+    def test_logged_in_uses_correct_template(self):
+        login=self.client.login(username='testuser1', password='P@ssw0rd1')
+        response=self.client.get(reverse('newresource'))
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'Club/newresource.html')
